a30523647f41476cdf48328aa3b167b69d55615b
balan <balan@victime.tp5.myguest.virtualbox.org>
2022-11-08 13:02:07 +0100

    test

--- tp5.py
+++ tp5.py
@@ -1,6 +1,6 @@
 import scapy.all as scapy
 
-ans=scapy.srp(scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst="10.33.19.0/22"),timeout=2, iface="wlan0")
+ans=scapy.srp(scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst="10.0.2.0/22"),timeout=2, iface="enp0s3")
 
 
 
