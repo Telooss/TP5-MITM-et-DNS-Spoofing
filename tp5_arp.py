de3506315680184c4b77ca9039e3360bd2c12666
balan <balan@victime.tp5.myguest.virtualbox.org>
2022-11-12 19:00:04 +0100

    FIX

--- tp5_arp.py
+++ tp5_arp.py
@@ -1,6 +1,6 @@
 import scapy.all as scapy
 
-result = scapy.srp((scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst="193.250.129.0/24")), timeout=3, verbose=0)[0]
+result = scapy.srp((scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst="10.4.1.0/24")), timeout=3, verbose=0)[0]
 
 print("Online IPs:")
 ip=[]
