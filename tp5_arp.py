7d13762c77edfae3656c17212e7cb16556165917
unknown <alan.waret24@gmail.com>
2022-11-12 18:58:32 +0100

    Update ARP

--- tp5_arp.py
+++ tp5_arp.py
@@ -1,10 +1,25 @@
-import scapy.all as scapy
+import sys
+from datetime import datetime
+from scapy.all import srp,Ether,ARP,conf 
 
-result = scapy.srp((scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst="193.250.129.0/24")), timeout=3, verbose=0)[0]
 
-print("Online IPs:")
-ip=[]
-mac=[]
-for sent, received in result:
-    ip.append(received.psrc)
-    mac.append(received.hwsrc)
+def arp_scan(interface, ips):
+
+	print("[*] Scanning...") 
+	start_time = datetime.now()
+
+	conf.verb = 0 
+	ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), 
+		     timeout = 2, 
+		     iface = interface,
+		     inter = 0.1)
+
+	print ("\n[*] IP - MAC") 
+	for snd,rcv in ans: 
+		print(rcv.sprintf(r"%ARP.psrc% - %Ether.src%"))
+	stop_time = datetime.now()
+	total_time = stop_time - start_time 
+	print("\n[*] Scan Complete. Duration:", total_time)
+
+if __name__ == "__main__":
+    arp_scan(sys.argv[1], sys.argv[2])
