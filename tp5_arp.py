dd837816196a0f17a9524328e0ee3df5564f94d9
unknown <alan.waret24@gmail.com>
2022-11-12 18:53:50 +0100

    Update ARP

--- tp5_arp.py
+++ tp5_arp.py
@@ -2,7 +2,9 @@ import scapy.all as scapy
 
 result = scapy.srp((scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst="193.250.129.0/24")), timeout=3, verbose=0)[0]
 
-if ans:
-    for element in ans:
-        print("IP: {}".format(element))
-        print("MAC address: {}\n".format(element))
+print("Online IPs:")
+ip=[]
+mac=[]
+for sent, received in result:
+    ip.append(received.psrc)
+    mac.append(received.hwsrc)
