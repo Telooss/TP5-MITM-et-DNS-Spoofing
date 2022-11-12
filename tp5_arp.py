ded12e90764a83e9457d18c43efffb43b01a507a
unknown <alan.waret24@gmail.com>
2022-11-12 19:16:53 +0100

    Modification

--- tp5_arp.py
+++ tp5_arp.py
@@ -1,17 +1,15 @@
 import sys
 from datetime import datetime
-from scapy.all import srp,Ether,ARP,conf 
+import scapy.all as scapy
 
 
 
 print("[*] Scanning...") 
 start_time = datetime.now()
 
-conf.verb = 0 
-ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst = 10.4.1.0/24), 
-	     timeout = 2, 
-	     iface = enp0s8,
-	     inter = 0.1)
+scapy.conf.verb = 0 
+ans, unans = scapy.srp(scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst = "10.4.1.0/24"), timeout = 2, iface = "enp0s8", inter = 0.1)
+
 
 print ("\n[*] IP - MAC") 
 for snd,rcv in ans: 
