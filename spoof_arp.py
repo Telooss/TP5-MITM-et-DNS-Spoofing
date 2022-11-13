77784f63e13f23f1a5623c74652032f0fa4a762b
unknown <alan.waret24@gmail.com>
2022-11-13 21:25:57 +0100

    La ZOOONE

--- spoof_arp.py
+++ spoof_arp.py
@@ -13,13 +13,8 @@ def ARP_Spoof(Network = str):
 
     print("[*] Scan en cours [...]") 
 
-    frame = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-
-    arp_packet = frame/scapy.ARP(pdst=Network)
-
-
-    ans = scapy.srp(arp_packet, timeout=2)
-     # Scan ARP pour récupérer les adresses
+    ans= scapy.srp(scapy.Ether(dst="ff:ff:ff:ff:ff:ff")/scapy.ARP(pdst = Network), timeout = 2, inter = 0.1)[0]
+    # Scan ARP pour récupérer les adresses
     
     print ("\n[*] IP - MAC") 
 
